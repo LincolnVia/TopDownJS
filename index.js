e4ffbb8402cd94de230819b6cec6391f465cdebd
LincolnVia <71860413+LincolnVia@users.noreply.github.com>
2025-09-22 06:32:32 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const ctx = document.querySelector("canvas").getContext("2d");
+const playerSprite = new Image()
+playerSprite.src = "PlayerCycle.png"
 
 ctx.canvas.height = 500;
 ctx.canvas.width = 500;
 
 // Start the frame count at 1
-let level = "home";
+let level = "Home";
 var floor = 1;
-const Boxes = [];
 
+
+const HomeBoxes = [];
+const TownABoxes = [];
+const MedBay = [];
 
 
 
@@ -33,18 +38,28 @@ function CheckCollision()
 // Create a collection to hold the generated x coordinates
 
 
-const square = {
+class C_Player{
+constructor(x,y,w,h,image)
+{
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.image = image;
+  this.xVelocity = 0;
+  this.yVelocity = 0;
 
-  height: 32,
-  jumping: true,
-  width: 32,
-  x: 250,
-  xVelocity: 0,
-  y: 250,
-  yVelocity: 0,
-  color: "#8DAA9D"
+  
+}
+draw()
+{
+  ctx.drawImage(this.image,this.x,this.y,128,128,0,0,16,32);
+}
 
-};
+
+}
+
+
 class Box{
 constructor(x,y,w,h,color)
 {
@@ -62,12 +77,26 @@ draw()
   
 }
 
+
 }
-function CreateBox(x,y,w,h,color)
+
+
+function CreateWorld()
 {
-  const box = new Box(x,y,w,h,color);
-  Boxes.push(box);
+  const MedbayCounter = new Box(100,250,200,30,"Blue");
+  MedBay.push(MedbayCounter);
+
+
+  const HomeExit = new Box(150,0,200,20,"red");
+  HomeBoxes.push(HomeExit);
+
+//Town
+  const TownExit = new Box(150,0,200,20,"orange");
+  TownABoxes.push(TownExit);
+  
+
 }
+
 
 
 // Create the obstacles for each frame
@@ -106,115 +135,147 @@ const controller = {
   }
 
 };
+CreateWorld();
 
-CreateBox(150,0, 200,20,"black");
+let img2 = document.getElementById("sprite");
+
+const player = new C_Player(250,250,16,32,img2);
+
+let img = document.getElementById("myImage");
+
+function Input()
+{
+  if (controller.up  ) {
+
+    player.yVelocity -= .2 ;
+    
+
+  }
+  else if (controller.down) {
+
+    player.yVelocity += .2 ;
+
+    
+
+  }
+  else if (controller.right) {
+
+    player.xVelocity += 0.2;
+
+  }
+  else if (controller.left) {
+
+    player.xVelocity -= 0.2;
+
+  }
+
+  
+}
+function BorderCollision()
+{
+    if (player.y > 500 - player.height / 2) {
+
+    player.jumping = false;
+    player.y = 500 - player.height / 2;
+    player.yVelocity = 0;
+    
+
+  }
+  if (player.y < 0 +  player.height / 2 ) {
+
+    player.jumping = false;
+    player.y = 0 + player.height / 2 ;
+  }
+  if(player.x < 0 + player.width / 2)
+  {
+    player.x = 0 + player.width / 2;
+    player.xVelocity = 0;
+  }
+  if(player.x > 500 - player.width / 2 )
+  {
+    player.x = 500 - player.width / 2;
+    player.xVelocity = 0;
+  }
+
+
+}
+
 
 
 const loop = function () {
 
 //#region Falling Stopper
 // if square is falling below floor line
+  ctx.fillStyle = "wheat";
+  ctx.fillRect(0, 0, 1300, 600); // x, y, width, height
+
 
  
-  if (square.y > 500 - square.height / 2) {
 
-    square.jumping = false;
-    square.y = 500 - square.height / 2;
-    square.yVelocity = 0;
-    
-
-  }
-  if (square.y < 0 +  square.height / 2 ) {
-
-    square.jumping = false;
-    square.y = 0 + square.height / 2 ;
-  }
-  if(square.x < 0 + square.width / 2)
-  {
-    square.x = 0 + square.width / 2;
-    square.xVelocity = 0;
-  }
-  if(square.x > 500 - square.width / 2 )
-  {
-    square.x = 500 - square.width / 2;
-    square.xVelocity = 0;
-  }
-  
+  Input();
+  BorderCollision();
+  player.draw();
   //#endregion
 
 //#region Controller
-  if (controller.up  ) {
-
-    square.yVelocity -= .2 ;
-    
-
-  }
-  else if (controller.down) {
-
-    square.yVelocity += .2 ;
-    
-
-  }
-  else if (controller.right) {
-
-    square.xVelocity += 0.2;
-
-  }
-  else if (controller.left) {
-
-    square.xVelocity -= 0.2;
-
-  }
-
+  
   //#endregion
 
 //#region Gravity and Friction
   
-  square.x += square.xVelocity;
-  square.y += square.yVelocity;
-  square.xVelocity *= 0.9;// friction
-  square.yVelocity *= 0.9;// friction
+  player.x += player.xVelocity;
+  player.y += player.yVelocity;
+  player.xVelocity *= 0.9;// friction
+  player.yVelocity *= 0.9;// friction
   
  // Creates the backdrop for each frame
   //#region DrawMap
-  ctx.fillStyle = "wheat";
-  ctx.fillRect(0, 0, 1300, 600); // x, y, width, height
-
-  for(let i = 0; i < Boxes.length; i++)
+   
+   switch(level)
   {
-    Boxes[i].draw();
-  }
-
-  //#endregion
-  if(level == "home")
-  {
+  case "Home":
     ctx.fillText(level,150,350);
-  
- 
-  }
-  if(level == "town")
-  {
+
+    for(let i = 0; i < HomeBoxes.length; i++)
+    {
+      HomeBoxes[i].draw();
+    }
+      
+    // If Player Box collides with door
+    if(player.x > HomeBoxes[0].x && player.x < player.x + HomeBoxes[0].w && player.y > HomeBoxes[0].y && player.y < HomeBoxes[0].y + HomeBoxes[0].h * 2)
+    {
+      player.xVelocity = 0;
+      player.yVelocity = 0;
+      level = "Town";
+      console.log("Town");
+      player.y = 484;
+      player.x = 250;
+    }  
+
+
     
-  }
+    break;
+  case "Town":
+    ctx.fillText(level,150,350);
+    for(let i = 1; i < TownABoxes.length; i++)
+    {
+      TownABoxes[i].draw();
+    }
+    break;
+  case "Medbay":
+    for(let i = 0; i < MedBay.length; i++)
+    {
+      MedBay[i].draw();
+    }
+ 
+}
   //#endregion
   // Creates the backdrop for each frame
   
 
-  // Creates and fills the cube for each frame
-  ctx.fillStyle = square.color; ; // hex for cube color
-  ctx.beginPath();
-  ctx.rect(square.x - square.width / 2, square.y - square.height / 2 , square.width, square.height);
-
-  ctx.fill();
  
   //#endregion
   
-
-  ctx.font = "30px Arial";
-  ctx.fillText(floor, 1000, 100);
-  ctx.fillText(Math.trunc(square.x), square.x, square.y - 32);
-  ctx.fillText(Math.trunc(square.y), square.x + 64, square.y - 32);
-
 
 
   
